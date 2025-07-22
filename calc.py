6fbedaa2fc13be4f190ef217e9c4072c4ba04074
Pari Margu <pari.margu2010@gmail.com>
2025-07-22 16:12:43 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,4 +1,5 @@
 print("Define a add function")
 print("Define a sub function")
 print("Define a mul function")
-print("Define a mul function")
+print("Define a div function")
+print("Define a mod function")
